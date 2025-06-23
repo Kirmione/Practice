e869cb5b99938818d9ffb7c6bdee45ed0c8b16f1
Kirmione <kirill.timonin.02@mail.ru>
2025-06-23 11:49:49 +0600

    Add files via upload

---
+++ Practice/Practice2025/PriceCalculation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PriceCalculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
